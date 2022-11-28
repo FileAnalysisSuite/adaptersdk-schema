86b59c290cf3fd6772f068e5bf2ded28e37f6cbe
vgkusuma <vgkusuma@microfocus.com>
2022-11-28 13:33:39 -0800

    Yaml path update

--- dotnet/src/Microfocus.FAS.AdapterSdkSchema.Generator/SchemaSourceCreator.cs
+++ dotnet/src/Microfocus.FAS.AdapterSdkSchema.Generator/SchemaSourceCreator.cs
@@ -56,16 +56,10 @@ namespace MicroFocus.FAS.AdapterSdkSchema
 
         private static JsonNode ParseSchemaDefinition()
         {
-            string schemaFile = Path.Combine(
-                Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName,
-                "schemaDefinition.json.yaml");
-            //schemaFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase), "schemaDefinition.json.yaml");
-            //schemaFile = "..\\..\\..\\schemaDefinition.json.yaml";
-
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new(codeBase);
+            UriBuilder uri = new(Assembly.GetExecutingAssembly().CodeBase);
+            //UriBuilder uri = new(Assembly.GetExecutingAssembly().Location);
             string path = Uri.UnescapeDataString(uri.Path);
-            schemaFile = Path.Combine(Path.GetDirectoryName(path), "schemaDefinition.json.yaml");
+            string schemaFile = Path.Combine(Path.GetDirectoryName(path), "schemaDefinition.json.yaml");
 
             //schemaFile = "schemaDefinition.json.yaml";
             Debug.WriteLine("Loading schema definition from : " + schemaFile);
