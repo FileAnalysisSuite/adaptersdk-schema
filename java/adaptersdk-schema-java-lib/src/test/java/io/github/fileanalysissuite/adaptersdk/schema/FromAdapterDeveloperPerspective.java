0b03e8817ce423cf588e222065f2b3daca413db0
vgkusuma <vgkusuma@microfocus.com>
2023-01-24 16:36:54 -0800

    Flatten values

--- java/adaptersdk-schema-java-lib/src/test/java/io/github/fileanalysissuite/adaptersdk/schema/FromAdapterDeveloperPerspective.java
+++ java/adaptersdk-schema-java-lib/src/test/java/io/github/fileanalysissuite/adaptersdk/schema/FromAdapterDeveloperPerspective.java
@@ -18,6 +18,7 @@ package io.github.fileanalysissuite.adaptersdk.schema;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Stream;
 
 // Say a developer is building a SalesForceAdapter
@@ -38,6 +39,12 @@ public class FromAdapterDeveloperPerspective
         System.out.println("Building TestSchemaObject : " + document);
         final TestSchemaObjectBuilder documentBuilder = new TestSchemaObjectBuilder(new MapSchemaObjectBuilder(document));
 
+        // Integer
+        documentBuilder.setColumnCount(3);
+        // Long
+        documentBuilder.setRootFileSize(418l);
+        // Boolean
+        documentBuilder.setHasAttachments(false);
         // Datetime
         documentBuilder.setDateArchived(Instant.now());
         // Single-valued string
@@ -59,7 +66,39 @@ public class FromAdapterDeveloperPerspective
             })
         );
 
-        // 2 level Nested objects, Json encoded
+        // multi- level Nested objects, Json encoded
+        documentBuilder.setColumns(Stream.of(
+            builder -> {
+                builder.setName("ColumnA");
+                builder.setReviewedBy("somebody");
+                builder.setReviewerComments("LGTM");
+                builder.setSelectedGrammarRule("GrammarRuleA");
+                builder.setStatus("REVIEWED");
+                builder.setType("TypeA");
+                builder.setGrammarMatches(Stream.of(
+                    mbuilder -> {
+                        mbuilder.setRules("RuleA-1", "RuleA-2");
+                        mbuilder.setWeight(50);
+                    }
+                ));
+            },
+            builder -> {
+                builder.setName("ColumnB");
+                builder.setReviewedBy("somebody");
+                builder.setReviewerComments("Needs changes");
+                builder.setSelectedGrammarRule("GrammarRuleB");
+                builder.setStatus("REVIEWED");
+                builder.setType("TypeB");
+                builder.setGrammarMatches(Stream.of(
+                    mbuilder -> {
+                        mbuilder.setRules("RuleB-1", "RuleB-2");
+                        mbuilder.setWeight(30);
+                    }
+                ));
+            })
+        );
+/*
+        // 2 level Nested objects, Flattened encoded -- not working
         documentBuilder.setEntities(Stream.of(
             builder -> {
                 builder.setEntityId("USA", "India");
@@ -69,7 +108,7 @@ public class FromAdapterDeveloperPerspective
                     mbuilder -> {
                         mbuilder.setContext("someMatchCtx");
                         mbuilder.setValue("USA");
-                        mbuilder.setScore(99.9);
+                        mbuilder.setScore(99.9); //Double
                     }
                 ));
             },
@@ -79,20 +118,114 @@ public class FromAdapterDeveloperPerspective
                 builder.setGrammarId("pii");
             })
         );
+*/
+        // multi-values Nested object, single-dimension - flattened
+        // Expected:
+        /*
+        ----------------
+        Json encoding
+        ----------------
+        METADATA_FILES =
+        [
+            {"CONTENT":"Reporter","EXTENSION":"xyz"},
+            {"CONTENT":"free form text from a non-templated region","EXTENSION":"abc"}
+        ]
+        ----------------
+        Flattened
+        ----------------
+        METADATA_FILES_0_CONTENT=
+        METADATA_FILES_0_EXTENSION=
 
-        // multi-values Nested object, flattened - not working
+        METADATA_FILES_1_CONTENT=
+        METADATA_FILES_1_EXTENSION=
+         */
+
+        /*
+        // Setting a single value for single-dimension multi-valued Nested object -- works
+        documentBuilder.setMetadataFiles(
+            builder -> {
+                builder.setContent("mdf-content1");
+                builder.setExtension("abc");
+            }
+        );
+        */
+
+        //Setting multiple values for single-dimension multi-valued Nested object -- works
         documentBuilder.setMetadataFiles(Stream.of(
             builder -> {
                 builder.setContent("mdf-content1");
-                builder.setExtension("xyz");
+                builder.setExtension("abc");
             },
             builder -> {
                 builder.setContent("mdf-content2");
-                builder.setExtension("abc");
+                builder.setExtension("xyz");
             })
         );
 
-        // multi-values Nested object, flattened
+        // multi-values Nested object, multi-dimension - flattened - not working
+        // Expected:
+        /*
+        ----------------
+        Json encoding
+        ----------------
+        OCR=
+        [
+            [
+                {"CONFIDENCE":41,"NAME":"Reporter","TYPE":"Name","VALUE":"John Doe"},
+                {"CONFIDENCE":42,"VALUE":"free form text from a non-templated region"}
+            ]
+        ]
+
+        OCR=
+        [
+            [
+                {"CONFIDENCE":41,"NAME":"Reporter","TYPE":"Name","VALUE":"John Doe"},
+                {"CONFIDENCE":42,"VALUE":"free form text from a non-templated region"}
+            ],
+            [
+                {"CONFIDENCE":412,"NAME":"Reporter2","TYPE":"Name2","VALUE":"John Doe2"},
+                {"CONFIDENCE":422,"VALUE":"free form text from a non-templated region2"}
+            ]
+        ]
+        ----------------
+        Flattened
+        ----------------
+        OCR_0_0_CONFIDENCE=41
+        OCR_0_0_NAME=Reporter
+        OCR_0_0_TYPE=Name
+        OCR_0_0_VALUE=John Doe
+
+        OCR_0_1_CONFIDENCE=42
+        OCR_0_1_VALUE=free form text from a non-templated region
+
+        OCR_1_0_CONFIDENCE=412
+        OCR_1_0_NAME=Reporter2
+        OCR_1_0_TYPE=Name2
+        OCR_1_0_VALUE=John Doe2
+
+        OCR_1_1_CONFIDENCE=422
+        OCR_1_1_VALUE=free form text from a non-templated region2
+        */
+        // Setting single value for multi-valued Nested object, multi-dimension - flattened - not working
+        documentBuilder.setOcr(
+            listBuilder -> { listBuilder.set(
+                Stream.of(
+                    builder -> {
+                        builder.setName("Reporter");
+                        builder.setType("Name");
+                        builder.setValue("John Doe");
+                        builder.setConfidence(41);
+                    },
+                    builder -> {
+                        builder.setConfidence(42);
+                        builder.setValue("free form text from a non-templated region");
+                    }
+                )
+            );
+            }
+        );
+        // TODO: Setting multi-values Nested object, multi-dimension - flattened - not working
+        /*
         documentBuilder.setOcr(Stream.of(
             listBuilder -> { listBuilder.set(
                 Stream.of(
@@ -124,8 +257,10 @@ public class FromAdapterDeveloperPerspective
                 )
             );
             })
-        );
+        );*/
 
-        System.out.println("Built TestSchemaObject : " + document);
+        System.out.println("-------------   Built TestSchemaObject ---------------------\n");
+        final Map<String, Object> treeMap = new TreeMap<String, Object>(document);
+        treeMap.entrySet().forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
     }
 }
